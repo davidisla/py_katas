4e45c3f4cdd72af7746c637034c9b00d149141e3
David Isla <d.isla@ibermatica.com>
2022-09-06 20:40:36 +0200

    RED: Should return buzz for numbers multiples of 5

--- test/test_fizzbuzz.py
+++ test/test_fizzbuzz.py
@@ -13,3 +13,9 @@ def test_return_fizz_for_multiples_of_3():
     assert fizzbuzz.do(9) == "fizz"
     assert fizzbuzz.do(12) == "fizz"
     assert fizzbuzz.do(21) == "fizz"
+
+def test_return_buzz_for_multiples_of_5():
+    assert fizzbuzz.do(10) == "buzz"
+    assert fizzbuzz.do(20) == "buzz"
+    assert fizzbuzz.do(55) == "buzz"
+    
