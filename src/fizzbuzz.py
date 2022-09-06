43e5281669e3478cea53b4dc66af6d597a3051b5
David Isla <d.isla@ibermatica.com>
2022-09-06 21:05:48 +0200

    REFACTOR: Added a isMultipleOf method to improve readability

--- src/fizzbuzz.py
+++ src/fizzbuzz.py
@@ -1,10 +1,16 @@
 class fizzbuzz():
 
-    def do(value):
-        if (value % 3) == 0:
+    @classmethod
+    def do(cls, value):
+        if cls.isMultipleOf(value, 3):
             return "fizz"
 
-        if (value % 5) == 0:
+        if cls.isMultipleOf(value, 5):
             return "buzz"
 
         return str(value)
+
+    @classmethod
+    def isMultipleOf(cls, value, module):
+        if (value % module) == 0:
+            return True
