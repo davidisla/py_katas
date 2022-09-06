dc6b2b9eb3e64a3ed709317f807e177974b12795
David Isla <d.isla@ibermatica.com>
2022-09-06 20:33:13 +0200

    RED: Should return fizz for multiples of 3

--- test/test_fizzbuzz.py
+++ test/test_fizzbuzz.py
@@ -8,3 +8,8 @@ def test_return_fizz_for_3():
 
 def test_return_buzz_for_5():
     assert fizzbuzz.do(5) == "buzz"
+
+def test_return_fizz_for_multiples_of_3():
+    assert fizzbuzz.do(9) == "fizz"
+    assert fizzbuzz.do(12) == "fizz"
+    assert fizzbuzz.do(21) == "fizz"
